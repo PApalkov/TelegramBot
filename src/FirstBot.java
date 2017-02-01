d8b0b8c2f797596b4ff72e0c4dbbb7dc5eee77e8
pavel <apalkov.pasha@yandex.ru>
2017-02-01 18:39:53 +0300

    Самая актуальная версия

--- src/FirstBot.java
+++ src/FirstBot.java
@@ -108,7 +108,7 @@ public class FirstBot extends TelegramLongPollingCommandBot {
 
     @Override
     public String getBotUsername() {
-        return "MyFirstTelegramBot";
+        return "MyFirstBot";
     }
 
     @Override
